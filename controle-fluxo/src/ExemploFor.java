6c64795ecff1c248298ddb127c94732ae42a3eca
Jajars <jailson.marques91@gmail.com>
2023-10-04 16:39:16 -0300

    aulas for, while e do-while

---
+++ controle-fluxo/src/ExemploFor.java
@@ -0,0 +1,12 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for (int carneirinhos = 1; carneirinhos <=20; carneirinhos++){
+            if (carneirinhos == 1)
+            System.out.println(carneirinhos + " carneirinho");
+            else
+            System.out.println(carneirinhos + " carneirinhos");
+        }
+
+        System.out.println("Joãozinho dormiu");
+    }
+}
