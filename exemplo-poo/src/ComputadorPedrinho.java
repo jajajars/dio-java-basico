9f9111efedc03c00eca85f9a0e863804a9fc9bf6
Jajars <jailson.marques91@gmail.com>
2023-10-11 15:38:52 -0300

    Adicionado exemplos de POO

--- exemplo-poo/src/ComputadorPedrinho.java
+++ exemplo-poo/src/ComputadorPedrinho.java
@@ -1,12 +1,19 @@
 public class ComputadorPedrinho {
     public static void main(String[] args) {
 		//abrindo MSN Messenger
+        System.out.println("MSN");
 		MSNMessenger msn = new MSNMessenger();
-		
-		//msn.validarConectadoInternet();
 		msn.enviarMensagem();
-		//msn.salvarHistoricoMensagem();
-		
 		msn.receberMensagem();
+
+        System.out.println("Telegram");
+        Telegram telegram = new Telegram();
+        telegram.enviarMensagem();
+        telegram.receberMensagem();
+
+        System.out.println("Messenger");
+        FacebookMessenger messenger = new FacebookMessenger();
+        messenger.enviarMensagem();
+        messenger.receberMensagem();
 	}
 }
