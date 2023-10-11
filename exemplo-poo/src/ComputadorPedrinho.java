4dbfe05195bf3533c8d69ae9539c8455a0c48df0
Jajars <jailson.marques91@gmail.com>
2023-10-11 15:16:54 -0300

    criadas classes ComputadorPedrinho e MSNMessenger

---
+++ exemplo-poo/src/ComputadorPedrinho.java
@@ -0,0 +1,12 @@
+public class ComputadorPedrinho {
+    public static void main(String[] args) {
+		//abrindo MSN Messenger
+		MSNMessenger msn = new MSNMessenger();
+		
+		//msn.validarConectadoInternet();
+		msn.enviarMensagem();
+		//msn.salvarHistoricoMensagem();
+		
+		msn.receberMensagem();
+	}
+}
