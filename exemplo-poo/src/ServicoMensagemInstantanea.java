be5503ba388955f6d4249f7b4b1f7824f6f8aa28
Jajars <jailson.marques91@gmail.com>
2023-10-11 15:44:47 -0300

    feitas alterações para exemplo de abstração

--- exemplo-poo/src/ServicoMensagemInstantanea.java
+++ exemplo-poo/src/ServicoMensagemInstantanea.java
@@ -1,16 +1,4 @@
-public class ServicoMensagemInstantanea {
-    public void enviarMensagem() {
-        validarConectadoInternet();
-		System.out.println("Enviando mensagem");
-        salvarHistoricoMensagem();
-	}
-	public void receberMensagem() {
-		System.out.println("Recebendo mensagem");
-	}
-	private void validarConectadoInternet() {
-		System.out.println("Validando se está conectado a internet");
-	}
-	private void salvarHistoricoMensagem() {
-		System.out.println("Salvando o histórico da mensagem");
-	}
+public abstract class ServicoMensagemInstantanea {
+    public abstract void enviarMensagem();
+	public abstract void receberMensagem();
 }
